b59e535a9fc8dc2b79c4d69e778645a4769aceb8
Jayde Jeong <yljung.jay@gmail.com>
2024-03-14 22:40:37 +0900

    Initial Commit

---
+++ CustomTabBarView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CustomTabBarView
+//
+//  Created by Yurim Jayde Jeong on 3/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomTabBarView/CustomTabBarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarViewApp.swift
+//  CustomTabBarView
+//
+//  Created by Yurim Jayde Jeong on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
